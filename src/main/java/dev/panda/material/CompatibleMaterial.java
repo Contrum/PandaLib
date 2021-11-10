0f7ae2b7361c6ee7d6a6e098743a185f25f88dd7
RisasDev <mauriciouch15@gmail.com>
2021-11-09 22:17:03 -0300

    Update compatible material

--- src/main/java/dev/panda/material/CompatibleMaterial.java
+++ src/main/java/dev/panda/material/CompatibleMaterial.java
@@ -13,53 +13,28 @@ import org.bukkit.Material;
 public enum CompatibleMaterial {
 
     HUMAN_SKULL("SKULL_ITEM", "LEGACY_SKULL_ITEM"),
-    RED_WOOL("WOOL", "RED_WOOL"),
-    ORANGE_WOOL("WOOL", "ORANGE_WOOL"),
-    YELLOW_WOOL("WOOL", "YELLOW_WOOL"),
-    DARK_GREEN_WOOL("WOOL", "GREEN_WOOL"),
-    GREEN_WOOL("WOOL", "LIME_WOOL"),
-    DARK_BLUE_WOOL("WOOL", "BLUE_WOOL"),
-    BLUE_WOOL("WOOL", "LIGHT_BLUE_WOOL"),
-    AQUA_WOOL("WOOL", "CYAN_WOOL"),
-    PURPLE_WOOL("WOOL", "PURPLE_WOOL"),
-    PINK_WOOL("WOOL", "PINK_WOOL"),
-    WHITE_WOOL("WOOL", "WHITE_WOOL"),
-    DARK_GRAY_WOOL("WOOL", "GRAY_WOOL"),
-    GRAY_WOOL("WOOL", "LIGHT_GRAY_WOOL"),
-    BLACK_WOOL("WOOL", "BLACK_WOOL"),
-    SNOW_BALL("SNOW_BALL", "SNOWBALL"),
+    WOOL("WOOL", "LEGACY_WOOL"),
+    SNOW_BALL("SNOW_BALL", "LEGACY_SNOW_BALL"),
     CARPET("CARPET", "LEGACY_CARPET");
 
     private final String material8;
-    private final String material12;
     private final String material13;
 
-    CompatibleMaterial(String material8, String material12, String material13) {
+    CompatibleMaterial(String material8, String material13) {
         this.material8 = material8;
-        this.material12 = material12;
         this.material13 = material13;
     }
 
-    CompatibleMaterial(String material8, String material13) {
-        this(material8, material13, material13);
-    }
-
-    CompatibleMaterial(String material13) {
-        this(null, null, material13);
+    CompatibleMaterial(String material8) {
+        this(material8, null);
     }
 
     public Material getMaterial() {
-        if (Server.SERVER_VERSION_INT == 8) {
+        if (Server.SERVER_VERSION_INT <= 12) {
             return material8 == null ? Material.valueOf("SKULL_ITEM") : Material.valueOf(material8);
         }
-        else if (Server.SERVER_VERSION_INT > 9 && Server.SERVER_VERSION_INT < 13) {
-            return material12 == null ? Material.valueOf("LEGACY_SKULL_ITEM") : Material.valueOf(material12);
-        }
-        else if (Server.SERVER_VERSION_INT >= 13) {
-            return material13 == null ? Material.valueOf("LEGACY_SKULL_ITEM") : Material.valueOf(material13);
-        }
         else {
-            return null;
+            return material13 == null ? Material.valueOf("LEGACY_SKULL_ITEM") : Material.valueOf(material13);
         }
     }
 }
